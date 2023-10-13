adeeba7e949cf0fb2ced0cd59826db2b5b5ecf0a
Abdulaziz Pulatjonov <apulotjonov@cdti.uz>
2023-10-13 17:22:35 +0500

    [SmartStations]

---
+++ src/main/java/com/example/smartstations/SmartStationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartstations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartstations/SmartStationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartstations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
